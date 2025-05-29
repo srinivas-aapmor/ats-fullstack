672aed4450b860f94007d9de9cc014738238c670
unknown <admin@GPL-5CD9265JYD.godrejinds.com>
2025-05-29 08:10:50 +0530

    Resume List table, added table button for upload resume, scores, search candidate, export list, and matched skills. redirection on upload resume button click.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import routes from './configs/routesConfig';
-
 function App() {
 
   return (
@@ -11,6 +10,7 @@ function App() {
         <Routes>
           {routes.map(({ path, element }, index) => (
             <Route key={index} path={path} element={element} />
+            
           ))}
         </Routes>
       </div>
